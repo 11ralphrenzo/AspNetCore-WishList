0284a189f91cceee0e566a43de44fde8cdb40b26
rpitogo@fullscale.io <rpitogo@fullscale.io>
2022-07-21 22:01:40 +0800

    Added UseEndpoints

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -24,6 +24,7 @@ namespace WishList
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
             else
